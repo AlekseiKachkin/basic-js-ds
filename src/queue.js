cc8901f35c0ad14e714ff5168b97efa50d8881be
AlekseiKachkin <lex6164665@yandex.ru>
2022-05-03 00:54:22 +0300

    feat: add solution for stack and remove-from-list

--- src/queue.js
+++ src/queue.js
@@ -13,23 +13,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.head = null;    
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    if(!this.head) {
+      this.head = new ListNode(value);
+    } 
+    else {
+      let current = this.head;
+    
+      while(current.next){
+        current = current.next
+      }
+
+      current.next = new ListNode(value)
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.head) {
+      let current = this.head;    
+      this.head = this.head.next
+      
+      return current.value
+    }
+    else {
+      return null
+    }
   }
 }
+
+let queue = new Queue;
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7);
+console.log(queue.dequeue())
+console.log(queue)
 
 module.exports = {
   Queue
